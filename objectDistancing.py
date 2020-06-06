7d9d60bf37bb04496de3944557505e91fb2c848f
ThomasWWebb <thomaswebb98uk@gmail.com>
2020-06-06 11:17:44 +0100

    Submission

--- objectDistancing.py
+++ objectDistancing.py
@@ -1,10 +1,22 @@
 import cv2
 import math
 import numpy as np
+import matplotlib.pyplot as plt
 
 camera_focal_length_px = 399.9745178222656
 camera_focal_length_m = 4.8 / 1000      
 stereo_camera_baseline_m = 0.2090607502
+
+max_disparity = 128
+
+crop_disparity = True
+
+def selectiveMedian(disparityBox, height, width, detractHeight, detractWidth):
+    return np.nanmedian(disparityBox[math.floor(height*detractHeight):math.floor(height*(1 - detractHeight)),
+                                   math.floor(width*detractWidth):math.floor(width*(1 - detractWidth))])
+
+#################################################################
+
 
 def selectiveMean(disparityBox, height, width, detractHeight, detractWidth):
     return np.nanmean(disparityBox[math.floor(height*detractHeight):math.floor(height*(1 - detractHeight)),
@@ -54,9 +66,6 @@ def getGauss(s, x, y):
     
 def calculateDepth(disparity, left, top, width, height):
 
-    f = camera_focal_length_px;
-    B = stereo_camera_baseline_m;
-
     disparitySum = 0
     pixelCount = 0
 
@@ -66,142 +75,198 @@ def calculateDepth(disparity, left, top, width, height):
     
     box = disparity[top:top+height, left:left+width]
 
-    disparityAvg = selectiveMean(box, height, width, 0.1, 0.1)
-    #disparityAvg = np.nanmedian(box, None)
+    #disparityAvg = selectiveMean(box, height, width, 0.125, 0.125)
+    #disparityAvg = selectiveMedian(box, height, width, 0.125, 0.125)
     #disparityAvg = centerPixel(box)
-    #disparityAvg = gaussianSum(box, 1)
+    disparityAvg = gaussianSum(box, 1)
 
     if (disparityAvg > 0):
 
-        Z = (f * B) / disparityAvg;
-        return Z
+        depth = (camera_focal_length_px * stereo_camera_baseline_m) / disparityAvg;
+        return depth
 
-    return 0;
+    return 0
 
 ##############################################################
 
-def calculateDisparity(full_path_filename_left, imgL):
+def calculateDisparity(imgL, imgR):
     #calculates the disparity image for a pair of stereo images
+
+    stereoProcessor = cv2.StereoSGBM_create(minDisparity = 0,
+                                            numDisparities = max_disparity,
+                                            blockSize = 21)
+
+    #convert images to greyscale 
+    grayL = cv2.cvtColor(imgL,cv2.COLOR_BGR2GRAY)
+    grayR = cv2.cvtColor(imgR,cv2.COLOR_BGR2GRAY)
+
+    #to reduce the impact of variable illumination 
+    clahe = cv2.createCLAHE(clipLimit=10.0, tileGridSize=(8,8))
+    grayL = clahe.apply(grayL)
+    grayR = clahe.apply(grayR)
+
+    #grayL = cv2.equalizeHist(grayL)
+    #grayR = cv2.equalizeHist(grayR)
+
+    #raise to power to improve calculation
+    grayL = np.power(grayL, 0.75).astype('uint8')
+    grayR = np.power(grayR, 0.75).astype('uint8')
+
+    disparity = stereoProcessor.compute(grayL,grayR)
     
-    max_disparity = 64;
-                                            
-    crop_disparity = True
-    full_path_filename_right = (full_path_filename_left.replace("_L", "_R")).replace("left", "right");
+    #reduce noise
+    dispNoiseFilter = 5;
+    cv2.filterSpeckles(disparity, 0, 4000, max_disparity - dispNoiseFilter);
 
-    if ('.png' in full_path_filename_left):
+    _, disparity = cv2.threshold(disparity,0, max_disparity * 16, cv2.THRESH_TOZERO);
+    disparity_scaled = (disparity / 16.).astype(np.uint8);
 
-        imgR = cv2.imread(full_path_filename_right, cv2.IMREAD_COLOR)
+    #crops unique sections of each camera
+    if (crop_disparity):
+        width = np.size(disparity_scaled, 1);
+        disparity_scaled = disparity_scaled[0:390,135:width];
 
-        #convert images to greyscale 
-        grayL = cv2.cvtColor(imgL,cv2.COLOR_BGR2GRAY);
-        grayR = cv2.cvtColor(imgR,cv2.COLOR_BGR2GRAY);
-        
-        #raise to power to improve calculation
-        grayL = np.power(grayL, 0.75).astype('uint8');
-        grayR = np.power(grayR, 0.75).astype('uint8');
-
-        #to improve contrast
-        #grayL = cv2.equalizeHist(grayL)
-        #grayR = cv2.equalizeHist(grayR)
-
-        clahe = cv2.createCLAHE(clipLimit=10.0, tileGridSize=(8,8))
-        grayL = clahe.apply(grayL)
-        grayR = clahe.apply(grayR)
-
-        disparity = stereoProcessor.compute(grayL,grayR)
-        
-        #reduce noise
-        dispNoiseFilter = 5;
-        cv2.filterSpeckles(disparity, 0, 4000, max_disparity - dispNoiseFilter);
-
-        _, disparity = cv2.threshold(disparity,0, max_disparity * 16, cv2.THRESH_TOZERO);
-        disparity_scaled = (disparity / 16.).astype(np.uint8);
-
-        cv2.imshow("disparity", (disparity_scaled * (256. / max_disparity)).astype(np.uint8));
-
-        #crops unique sections of each camera
-        if (crop_disparity):
-            width = np.size(disparity_scaled, 1);
-            disparity_scaled = disparity_scaled[0:390,135:width];
-
-        return disparity_scaled
-
-    else:
-            print("-- files skipped (perhaps one is missing or not PNG)");
-            print();
+    return disparity_scaled
 
 #####################################################################
 
-def calculateDisparityWLS(full_path_filename_left, imgL):
+def calculateDisparityWLS(imgL, imgR):
     #calculates the disparity image for a pair of stereo images combined with a wls filter
+    #utilising the online tutorial https://docs.opencv.org/master/d3/d14/tutorial_ximgproc_disparity_filtering.html
+
+    #convert images to greyscale 
+    grayL = cv2.cvtColor(imgL,cv2.COLOR_BGR2GRAY);
+    grayR = cv2.cvtColor(imgR,cv2.COLOR_BGR2GRAY);
+
+    clahe = cv2.createCLAHE(clipLimit=10.0, tileGridSize=(8,8))
+    grayL = clahe.apply(grayL)
+    grayR = clahe.apply(grayR)
     
-    max_disparity = 112;
+    #raise to power to improve calculation
+    grayL = np.power(grayL, 0.75).astype('uint8');
+    grayR = np.power(grayR, 0.75).astype('uint8');
+
+    #to improve contrast
+    #grayL = cv2.equalizeHist(grayL)
+    #grayR = cv2.equalizeHist(grayR)
+
+    filterLambda = 80000
+    sigma = 1.2
+
+    #creates both matchers
+    leftSide = cv2.StereoSGBM_create(minDisparity = 0,
+                                        numDisparities = max_disparity,
+                                        blockSize = 11)
+    rightSide = cv2.ximgproc.createRightMatcher(leftSide)
+    #initialises the wls filter
+    wls = cv2.ximgproc.createDisparityWLSFilter(leftSide)
+    wls.setLambda(filterLambda)
+    wls.setSigmaColor(sigma)
+
+    #computes both disparity images
+    disparityL = leftSide.compute(grayL, grayR)
+    disparityR = rightSide.compute(grayR, grayL)
+
+    #applies speckle filtering to both disparity images
+    dispNoiseFilter = 5;
+    cv2.filterSpeckles(disparityL, 0, 4000, max_disparity - dispNoiseFilter);
+    cv2.filterSpeckles(disparityR, 0, 4000, max_disparity - dispNoiseFilter);
     
-    crop_disparity = True
-    full_path_filename_right = (full_path_filename_left.replace("_L", "_R")).replace("left", "right");
+    #thresholds and scales for display
+    _, disparityLScaled = cv2.threshold(disparityL,0, max_disparity * 16, cv2.THRESH_TOZERO);
+    disparityLShow = (disparityLScaled / 16).astype(np.uint8)
 
-    if ('.png' in full_path_filename_left):
+    #applies the wls filter to both disparity images to produce one image
+    disparity = wls.filter(disparityL, grayL, None, disparityR)
+    
+    #further speckle filtering to the final image, thresholding and scaling for distance calculation
+    cv2.filterSpeckles(disparity, 0, 4000, max_disparity - dispNoiseFilter);
+    _, disparity = cv2.threshold(disparity,0, max_disparity * 16, cv2.THRESH_TOZERO);
+    disparity_scaled = (disparity / 16).astype(np.uint8);
 
-        imgR = cv2.imread(full_path_filename_right, cv2.IMREAD_COLOR)
+    #crops unique sections of each camera
+    if (crop_disparity):
+        width = np.size(disparity_scaled, 1);
+        disparity_scaled = disparity_scaled[0:390,135:width];
 
-        #convert images to greyscale 
-        grayL = cv2.cvtColor(imgL,cv2.COLOR_BGR2GRAY);
-        grayR = cv2.cvtColor(imgR,cv2.COLOR_BGR2GRAY);
-        
-        #raise to power to improve calculation
-        grayL = np.power(grayL, 0.75).astype('uint8');
-        grayR = np.power(grayR, 0.75).astype('uint8');
+    return disparity_scaled
 
-        #to improve contrast
-        #grayL = cv2.equalizeHist(grayL)
-        #grayR = cv2.equalizeHist(grayR)
+##############################################################################
 
-        clahe = cv2.createCLAHE(clipLimit=10.0, tileGridSize=(8,8))
-        grayL = clahe.apply(grayL)
-        grayR = clahe.apply(grayR)
+def calculateDisparitySparse(queryImage, searchImage, left):
+    #calculates the disparity of a detected object using sparse stereo
+    #utilising your example and the online tutorials https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_feature2d/py_orb/py_orb.html
+    
+    #Adds a border to enable feature point matching
+    queryImage = cv2.copyMakeBorder(queryImage, 100, 100, 100, 100, cv2.BORDER_CONSTANT, 0)
+    searchImage = cv2.copyMakeBorder(searchImage, 100, 100, 100, 100, cv2.BORDER_CONSTANT, 0);
 
-        filterLambda = 80000
-        sigma = 1.2
-        
-        leftSide = cv2.StereoSGBM_create(minDisparity = 0,
-                                            numDisparities = max_disparity,
-                                            blockSize = 21,
-                                            P1 = 1,
-                                            P2 = 5,
-                                            disp12MaxDiff = 64,
-                                            preFilterCap = 12,
-                                            uniquenessRatio = 10,
-                                            speckleWindowSize = 50,
-                                            speckleRange = 2,
-                                            mode = 0)
-        rightSide = cv2.ximgproc.createRightMatcher(leftSide)
-        wls = cv2.ximgproc.createDisparityWLSFilter(leftSide)
-        wls.setLambda(filterLambda)
-        wls.setSigmaColor(sigma)
+    #convert images to greyscale 
+    queryImage = cv2.cvtColor(queryImage,cv2.COLOR_BGR2GRAY);
+    searchImage = cv2.cvtColor(searchImage,cv2.COLOR_BGR2GRAY);
 
-        disparityL = leftSide.compute(grayL, grayR)
+    clahe = cv2.createCLAHE(clipLimit=10.0, tileGridSize=(8,8))
+    #queryImage = clahe.apply(queryImage)
+    #searchImage = clahe.apply(searchImage)
 
-        cv2.imshow("disparity before", disparityL);
-        disparityR = rightSide.compute(grayR, grayL)
+    # Create detector
+    orb = cv2.ORB_create()
+    
+    # find the keypoints and descriptors with ORB
+    queryKeypoints, queryDescriptors = orb.detectAndCompute(queryImage,None)
+    searchKeypoints, searchDescriptors = orb.detectAndCompute(searchImage,None)
 
-        disparity = wls.filter(disparityL, grayL, None, disparityR)
-        
-        #reduce noise
-        dispNoiseFilter = 5;
-        cv2.filterSpeckles(disparity, 0, 4000, max_disparity - dispNoiseFilter);
+    FLANN_INDEX_LSH = 6
+    index_params= dict(algorithm = FLANN_INDEX_LSH,
+                       table_number = 6,
+                       key_size = 12,   
+                       multi_probe_level = 1)
 
-        _, disparity = cv2.threshold(disparity,0, max_disparity * 16, cv2.THRESH_TOZERO);
-        disparity_scaled = (disparity / 16.).astype(np.uint8);
+    search_params = dict(checks=50)
+    flann = cv2.FlannBasedMatcher(index_params,search_params)
+    matches = flann.knnMatch(queryDescriptors,searchDescriptors,k=2)
+    
+    # Need to draw only good matches, so create a mask
+    matchesMask = [[0,0] for i in range(0, len(matches))]
+    good = []
+    for i, pair in enumerate(matches):
+        try:
+            m, n = pair
+            if m.distance < 0.7*n.distance:
+                good.append(m)
+        except ValueError:
+            pass
+    queryKeypointsCoords = []
+    searchKeypointsCoords = []
 
-        cv2.imshow("disparity", (disparity_scaled * (256. / max_disparity)).astype(np.uint8));
+    for mat in good:
+        # Get the matching keypoints for each of the images
+        searchImageIndex = mat.trainIdx
+        queryImageIndex = mat.queryIdx
 
-        #crops unique sections of each camera
-        if (crop_disparity):
-            width = np.size(disparity_scaled, 1);
-            disparity_scaled = disparity_scaled[0:390,135:width];
+        #Get the coordinates of the matches
+        (x1,y1) = queryKeypoints[queryImageIndex].pt
+        (x2,y2) = searchKeypoints[searchImageIndex].pt
+        if y2 == y1:
+            queryKeypointsCoords.append(x1)
+            searchKeypointsCoords.append(x2)
 
-        return disparity_scaled
+    if not len(queryKeypointsCoords):
+        return 0
+    
+    return distanceFromCoords(queryKeypointsCoords,searchKeypointsCoords, left)
 
-    else:
-            print("-- files skipped (perhaps one is missing or not PNG)");
-            print();
+def distanceFromCoords(queryKeypointsCoords,searchKeypointsCoords, left):
+    disparitySum = 0
+    for index in range(0, len(queryKeypointsCoords)):
+        disparitySum += abs(queryKeypointsCoords[index] + left - searchKeypointsCoords[index])
+
+    disparityAvg = disparitySum / len(queryKeypointsCoords)
+    if (disparityAvg > 0):
+
+        depth = (camera_focal_length_px * stereo_camera_baseline_m) / disparityAvg;
+        return depth
+
+    return 0
+    
+    
--- objectRanging.py
+++ objectRanging.py
@@ -1,10 +1,14 @@
-import cv2
 import argparse
+import numpy as np
+import cv2
 import math
 import objectDistancing
-import numpy as np
-import glob
- 
+from os import listdir
+from os.path import isfile, join
+
+useWLS = True #enables WLS filtering
+useSparseStereo = False # enables sparse stereo instead of dense
+master_path_to_dataset = "C:/Users/thoma/Documents/Third Year Completed/Vision/TTBB-durham-02-10-17-sub10/left-images/"
 def drawPred(image, class_name, confidence, left, top, right, bottom, colour, distance):
     cv2.rectangle(image, (left, top), (right, bottom), colour, 1)
 
@@ -80,7 +84,6 @@ net.setPreferableTarget(cv2.dnn.DNN_TARGET_OPENCL)
 ################################################################################
 
 # define display window name
-
 windowName = 'Object Ranging'
 cv2.namedWindow(windowName, cv2.WINDOW_NORMAL)
 
@@ -88,91 +91,119 @@ cv2.namedWindow(windowName, cv2.WINDOW_NORMAL)
 
 cv2.namedWindow(windowName, cv2.WINDOW_NORMAL)
 
-skipToFile = "1506942549.475784_L"
-
+skipToFile = "1506942555.485817_L"
+#skipToFile = ""
 #applies object detection and ranging to each pair of images in the dataset
-for file in glob.glob("C:/Users/thoma/Documents/Vision/TTBB-durham-02-10-17-sub10/left-images/*.png"):
+
+fileList = listdir(master_path_to_dataset)
+for fileL in fileList:
+    
+    file = join(master_path_to_dataset, fileL)
+    #skips to a specific file if one is set
     if len(skipToFile) != 0 and not (skipToFile in file):
         continue
     elif len(skipToFile) !=0 and skipToFile in file:
-        skipToFile = ""
-    #file = "C:/Users/thoma/Documents/Vision/TTBB-durham-02-10-17-sub10/left-images/1506942473.484027_L.png"
-    frame = cv2.imread(file)
-
-    start_t = cv2.getTickCount()
-
-    # create a 4D tensor (OpenCV 'blob') from image frame (pixels scaled 0->1, image resized)
-    tensor = cv2.dnn.blobFromImage(frame, 1/255, (inpWidth, inpHeight), [0,0,0], 1, crop=False)
-
-    # set the input to the CNN network
-    net.setInput(tensor)
-
-    # runs forward inference to get output of the final output layers
-    results = net.forward(output_layer_names)
-
-    # remove the bounding boxes with low confidence
-    confThreshold = 0
-    classIDs, confidences, boxes = postprocess(frame, results, confThreshold, nmsThreshold)
+        skipToFile = ""  
     
-    #compute the disparity image of the stereo pair
-    disparityImg = objectDistancing.calculateDisparityWLS(file, frame)
+    fullRightFile = (file.replace("_L", "_R")).replace("left", "right");
+    fileR = fileL.replace("_L", "_R")
 
-    boxDistances = []
+    if isfile(fullRightFile) and isfile(file):
 
-    for detected_object in range(0, len(boxes)):
-        box = boxes[detected_object]
-        left = box[0]
-        top = box[1]
-        width = box[2]
-        height = box[3]
-        right = left + width
-        bottom = top + height
+        #read in the stereo image pair    
+        frame = cv2.imread(file)
+        imgR = cv2.imread(fullRightFile, cv2.IMREAD_COLOR)
+
+        start_t = cv2.getTickCount()
+
+        # create a 4D tensor (OpenCV 'blob') from image frame (pixels scaled 0->1, image resized)
+        tensor = cv2.dnn.blobFromImage(frame, 1/255, (inpWidth, inpHeight), [0,0,0], 1, crop=False)
+        # set the input to the CNN network
+        net.setInput(tensor)
+        # runs forward inference to get output of the final output layers
+        results = net.forward(output_layer_names)
+        # remove the bounding boxes with low confidence
+        confThreshold = 0
+        classIDs, confidences, boxes = postprocess(frame, results, confThreshold, nmsThreshold)
         
-        if left + width // 2 >= 135 and top + height // 2 <= 390:
+        #compute the disparity image of the stereo pair'
+        if not useSparseStereo:
+            if useWLS:
+                disparityImg = objectDistancing.calculateDisparityWLS(frame, imgR)
+            else:
+                disparityImg = objectDistancing.calculateDisparity(frame, imgR)
 
-            #calculate the distance of the object from the detection box
-            distance = objectDistancing.calculateDepth(disparityImg, left,top, width, height)
-            distance =  round(abs(distance), 3)
-            boxDistances.append(distance)
-            #drawPred(frame, classes[classIDs[detected_object]], confidences[detected_object], left, top, right, bottom, (255, 178, 50), distance)
-        else:
-            boxDistances.append(0)
+        boxDistances = []
 
-    for detected_object in range(0, len(boxes)):
-        box = boxes[detected_object]
-        left = box[0]
-        top = box[1]
-        width = box[2]
-        height = box[3]
-        right = left + width
-        bottom = top + height
+        for detected_object in range(0, len(boxes)):
+            box = boxes[detected_object]
+            left = box[0]
+            top = box[1]
+            width = box[2]
+            height = box[3]
+            right = left + width
+            bottom = top + height
+
+            distance = 0
+
+            detectedObject = frame[top:top+height, left:right]
+            
+            #only estimate the distance of objects for which the majority of the box is within the disparity image
+            if left + width // 2 >= 135 and top + height // 2 <= 390 and len(detectedObject) > 0:
+
+                #calculate the distance of the object from the detection box
+                if useSparseStereo:
+                    distance = objectDistancing.calculateDisparitySparse(detectedObject,imgR[top:bottom, :], left)
+                else:
+                    distance = objectDistancing.calculateDepth(disparityImg, left,top, width, height)
+                    cv2.imshow("disparity after", (disparityImg * (256. / 128)).astype(np.uint8));
+
+                distance =  round(abs(distance), 3)
+                boxDistances.append(distance)
+            else:
+                boxDistances.append(distance)
+
+        minDistance = math.inf
+        #display the estimations on the image, done after all distance estimations to prevent earlier estimation displays effecting later estimations in close proximity
+        for detected_object in range(0, len(boxes)):
+            box = boxes[detected_object]
+            left = box[0]
+            top = box[1]
+            width = box[2]
+            height = box[3]
+            right = left + width
+            bottom = top + height
+            
+            if left + width // 2 >= 135 and top + height // 2 <= 390: #and boxDistances[detected_object] > 0:
+                if minDistance > boxDistances[detected_object] and boxDistances[detected_object] > 0:
+                    minDistance = boxDistances[detected_object]
+                drawPred(frame, classes[classIDs[detected_object]], confidences[detected_object], left, top, right, bottom, (255, 178, 50), boxDistances[detected_object])
+
+
+        if minDistance == math.inf:
+            minDistance = 0
+        print(fileL)
+        print("{} : nearest detected scene object ({}m)".format(fileR, minDistance))
+
+        #efficiency information
+        t, _ = net.getPerfProfile()
+        label = 'Inference time: %.2f ms' % (t * 1000.0 / cv2.getTickFrequency())
+        cv2.putText(frame, label, (0, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
         
-        if left + width // 2 >= 135 and top + height // 2 <= 390:
-            drawPred(frame, classes[classIDs[detected_object]], confidences[detected_object], left, top, right, bottom, (255, 178, 50), boxDistances[detected_object])
+        # display image
+        cv2.imshow(windowName,frame)
+        cv2.setWindowProperty(windowName, cv2.WND_PROP_FULLSCREEN, False )
 
-    # Put efficiency information. The function getPerfProfile returns the overall time for inference(t) and the timings for each of the layers(in layersTimes)
-    t, _ = net.getPerfProfile()
-    label = 'Inference time: %.2f ms' % (t * 1000.0 / cv2.getTickFrequency())
-    cv2.putText(frame, label, (0, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
+        # stop the timer and convert to ms. (to see how long processing and display takes)
+        stop_t = ((cv2.getTickCount() - start_t)/cv2.getTickFrequency()) * 1000
 
-    # display image
-    cv2.imshow(windowName,frame)
-    #cv2.imshow("disparity after", disparityImg);
-    cv2.imshow("disparity after", (disparityImg * (256. / 64)).astype(np.uint8));
-    cv2.setWindowProperty(windowName, cv2.WND_PROP_FULLSCREEN, False )
+        key = cv2.waitKey(5)
 
-    # stop the timer and convert to ms. (to see how long processing and display takes)
-    stop_t = ((cv2.getTickCount() - start_t)/cv2.getTickFrequency()) * 1000
-
-    # start the event loop + detect specific key strokes
-    # wait 40ms or less depending on processing time taken (i.e. 1000ms / 25 fps = 40 ms)
-    key = cv2.waitKey(max(2, 40 - int(math.ceil(stop_t)))) & 0xFF
-
-    # if user presses "x" then exit  / press "f" for fullscreen display
-    if (key == ord('x')):
-        break
-    elif (key == ord('f')):
-        args.fullscreen = not(args.fullscreen)
+        # if user presses "x" then exit  / press "f" for fullscreen display
+        if (key == ord('x')):
+            break
+        elif (key == ord('f')):
+            args.fullscreen = not(args.fullscreen)
 
 cv2.destroyAllWindows()
 
